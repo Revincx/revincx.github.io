b3c0d34f93f16e4ef3258afb8381dca235191767
Revincx <revincx@outlook.com>
2021-02-05 16:01:23 +0800

    Site updated: 2021-02-05 16:01:23

--- js/inject.js
+++ js/inject.js
@@ -3,3 +3,11 @@
 //   full_page[0].style.background = "transparent";
 // }
 
+// imgs=document.getElementsByTagName('img')
+// for(i=0;i<imgs.length;i++)
+// {
+//     if (imgs[i].src.indexOf('blog.revincx.icu') != -1)
+//     {
+//         imgs[i].src.replace('blog.revincx.icu','cdn.jsdelivr.net/gh/Revincx/revincx.github.io@master');
+//     }
+// }
