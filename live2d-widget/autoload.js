60a39dc734cce0e10e1eebd672909802a3b0a54a
Revincx <revincx@outlook.com>
2021-03-04 18:27:08 +0800

    Site updated: 2021-03-04 18:27:06

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -34,7 +34,8 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://cdn.jsdelivr.net/gh/Revincx/live2d_api@master/"
+			cdnPath: "https://cdn.jsdelivr.net/gh/Revincx/live2d_api@master/",
+			// cdnPath: "https://live2d-api.vercel.app/"
 		});
 	});
 }
