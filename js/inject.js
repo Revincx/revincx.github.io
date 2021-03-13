bb43d7614310c32fe29d0c565cba6c2cc018ef71
Revincx <revincx@outlook.com>
2021-03-13 14:36:16 +0800

    Site updated: 2021-03-13 14:36:15

--- js/inject.js
+++ js/inject.js
@@ -11,3 +11,10 @@
 //         imgs[i].src.replace('blog.revincx.icu','cdn.jsdelivr.net/gh/Revincx/revincx.github.io@master');
 //     }
 // }
+
+var date = new Date()
+
+if(date.getDate() == 4 && date.getMonth() == 3){
+    document.getElementsByTagName('html')[0].style.filter = "grayscale(1)"
+    console.log("清明节安康。")
+}
